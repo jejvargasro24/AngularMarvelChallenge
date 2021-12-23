e7efc295b71d568974dd3d0d3f94e946eaf3a594
PEVAARjeison.vargas <jeison.vargas@pevaar.com>
2021-12-22 22:40:02 -0500

    Another change to try and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: "http://gateway.marvel.com/v1/public",
+  apiKey: "85fd426db64c9b02b6c9959420842bc9",
+  hash: "4512943f7f5646452082686fff7808b1"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiBaseUrl: "http://gateway.marvel.com/v1/public",
   apiKey: "85fd426db64c9b02b6c9959420842bc9",
   hash: "4512943f7f5646452082686fff7808b1"
